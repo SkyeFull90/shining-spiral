c3c9feea3538d3a3bb591e1d0d66f5f529ca42fd
skye <full.skyl@hotmail.com>
2024-05-14 16:29:18 -0600

    starting to build the functionality of the node server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
 import dotenv from "dotenv"
 import express from "express"
 import app from "express"
+import config from "./config.js"
+import mongoose from "mongoose"
 /*
- import config from "./config.js"
  import catRoutes from "./routes/catRoute.js"
  import userRoutes from "./routes/userRoute.js"
- import mongoose from "mongoose"
  import passport from "passport"
  import {Strategy as LocalStrategy} from "passport-local"
+ import User from "./models/userModel.js"
 */ 
+dotenv.config();
+const port = process.env.PORT || 3000
+config()
+//app.use(express.static('dist'))
+
+app.use(express.urlencoded({ extended: true}))
+
+app.listen(port, () => {
+    console.log(`Listening on port http://localhost:${port}`)
+})
