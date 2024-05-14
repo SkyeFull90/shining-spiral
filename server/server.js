fec35eecfda1367cbc2ad6b7225e2113211dfc55
skye <full.skyl@hotmail.com>
2024-05-14 15:56:40 -0600

    builing the server

--- server/server.js
+++ server/server.js
@@ -1,2 +1,11 @@
-require("dotenv").config();
-const express = require("express")
+import dotenv from "dotenv"
+import express from "express"
+import app from "express"
+/*
+ import config from "./config.js"
+ import catRoutes from "./routes/catRoute.js"
+ import userRoutes from "./routes/userRoute.js"
+ import mongoose from "mongoose"
+ import passport from "passport"
+ import {Strategy as LocalStrategy} from "passport-local"
+*/ 
